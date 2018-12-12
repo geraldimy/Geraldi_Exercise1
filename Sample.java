15a9448c328f32302d4024aeb3386e23838463e9
geraldimy <geraldimy21@gmail.com>
2018-12-12 15:06:21 +0700

    This is third commit

--- Sample.java
+++ Sample.java
@@ -1,5 +1,8 @@
 public class sample {
     public static void main (String args[]){
-        system.out.println ("This Is just a sample of java file for giit")
+        system.out.println ("This Is just a sample of java file for giit");
+	system.out.println("We are adding the output lines to see the changes");
+	system.out.println("This is a second modification in java file to see the stage and unstaged changes");
+	
     }
 }
